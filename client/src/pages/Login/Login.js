4fef401d33509fff39797fbf043a83fa4cc0f4d9
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-12-22 22:23:19 +0500

    login

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,107 @@
+import React, {useState } from "react";
+// import { useForm } from "react-hook-form";
+// import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers/yup";
+import styles from "./Login.module.css";
+// import UsernameIcon from "../../assets/icons/UsernameIcon";
+import LoginMainIcon from "../../assets/icons/LoginMainIcon";
+import PasswordIcon from "../../assets/icons/PasswordIcon";
+// const schema = yup.object().shape({
+// 	username: yup
+// 		.string()
+// 		.trim()
+// 		.required("Username bo'sh bo'lishi mumkin emas")
+// 		.min(5, "Username xato kiritildi")
+// 		.max(20, "Username xato kiritildi"),
+// 	password: yup
+// 		.string()
+// 		.trim()
+// 		.required("Parol bo'sh bo'lishi mumkin emas")
+// 		.min(6, "Parol  xato kiritildi")
+// 		.max(20, "Parol xato kiritildi"),
+// });
+
+function Login() {
+	// const {
+	// 	register,
+	// 	handleSubmit,
+	// 	formState: { errors },
+	// } = useForm({
+	// 	resolver: yupResolver(schema),
+	// });
+	const [typeState, setTypeState] = useState(null);
+	const typeChangeHandler = () => {
+		setTypeState(!typeState);
+	};
+	const login = async (data) => {
+	    console.log(data)
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles["left-page"]}>
+				<LoginMainIcon classname={styles["login-icon"]} />
+			</div>
+			<div className={styles["right-page"]}>
+				<div className={styles["right-page-main-content"]}>
+					<h1>Tizimga Kirish</h1>
+					<form onSubmit={
+						// handleSubmit(login)
+					()=>{}	}>
+						<div>
+							<label htmlFor="username">
+								<div className={styles.spandiv}>
+									{/* <span className={styles["input-name"]}>Username </span> */}
+									
+								</div>
+								Username
+								<input
+									// style={{ borderBottom: errors.username && "1px solid red" }}
+									type="text"
+									id="username"
+									name="username"
+									// {...register("username")}
+								/>
+							</label>
+							{/* {errors.username && (
+								<p style={{ color: "red" }}>{errors.username.message}</p>
+							)} */}
+						</div>
+						<div className={styles.password}>
+							<label htmlFor="password">
+								<div className={styles.spandiv}>
+									{/* <span className={styles["input-name"]}>Parol</span> */}
+									<div className={styles.changeType}>
+										<div onClick={typeChangeHandler}>
+											<PasswordIcon />
+										</div>
+									</div>
+								</div>
+                               {/* Parol */}
+								<input
+									// style={{ borderBottom: errors.password && "1px solid red" }}
+									type={typeState ? "text" : "password"}
+									name="password"
+									id="password"
+									// {...register("password")}
+								/>
+							</label>
+							{/* {errors.password && (
+								<p style={{ color: "red" }}>{errors.password.message}</p>
+							)} */}
+						</div>
+						<div style={{ marginTop: "1rem" }}>
+
+							<button size="medium" name="btn" type="submit" >
+								Kirish
+							</button>
+
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
