03e4c8244842790b25d8a5c3abe5548f0a08aae7
dilnoza2808 <alilova2808@gmail.com>
2022-12-25 20:41:48 +0500

    general componentlar yasaldi

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import Login from "./pages/Login/Login";
+import Card from "./general/Card/Card"
+import ichimlik from "./assets/images/Qaynoq-ichimliklar.png";
+import Taomlar from "./assets/images/Taomlar.png";
+import Button from "./general/Button/Button"
 
 function App() {
 	return (
 		<>
-		<Login/>
+			<Login />
 		</>
 	);
 }
