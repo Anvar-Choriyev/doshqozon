8c61c4b11351e30a62a826289ca8a175bb5bae8b
Shodiyor Ergashev <mybusinesskebap@gmail.com>
2022-12-25 15:27:23 +0500

    missed auth router added

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const authRouter = require("../server/modules/auth/authRouter");
 const categoryRouter = require("../server/modules/category/categoryRouter");
 const userRouter = require("../server/modules/user/userRouter");
 app.use(express.json());
 app.use(require("cors")());
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
 module.exports = app;
