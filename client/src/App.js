a6e686028984829ee41306445788547d0eb254c9
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-12-26 15:32:42 +0500

    modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Categories from "./pages/Categories/Categories";
 import navbar from "./utils/navbar";
+import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
 
 function App() {
-	return (
+	const token = localStorage.getItem("token");
+	const user = localStorage.getItem("user");
+	const navigate=useNavigate()
+
+	useEffect(() => {
+		if (!token) {
+		  navigate("/login");
+		}
+		// ctx.setAppData({
+		//   user: JSON.parse(user),
+		//   token,
+		//   isAuth: token?.trim().length > 0,
+		// });
+	  }, [token]);
+	return (<>
+      <ToastContainer />
 		<Routes>
 			{navbar.map(({ id, path, element }) => <Route key={id()} path={path} element={element} />)}
 			<Route path='*' element={<Navigate to={'/'} />} />
 			<Route path="/menu/categories" element={<Categories/>}/>
-		</Routes>
+		</Routes></>
 	);
 }
 
