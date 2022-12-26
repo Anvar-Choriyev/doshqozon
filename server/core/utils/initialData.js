6e7c469d6c309bf97f701e908534f640b96002bd
Abdumalik <e.a1994@mail.ru>
2022-12-26 20:03:02 +0500

    update

--- server/core/utils/initialData.js
+++ server/core/utils/initialData.js
@@ -7,31 +7,30 @@ const foodsJson = require("../constants/foods.json");
 const User = require("../../modules/user/User");
 const userRoles = require("../constants/userRole");
 module.exports = catchAsync(async () => {
-	const countCategories = await Category.count();
+  const countCategories = await Category.count();
 
-	if (countCategories === 0) {
-		await Category.bulkCreate(categoriesJson);
-	}
-	const countFoods = await Food.count();
+  if (countCategories === 0) {
+    await Category.bulkCreate(categoriesJson);
+  }
+  const countFoods = await Food.count();
 
-	if (countFoods === 0) {
-		await Food.bulkCreate(foodsJson);
-	}
-	const adminCount = await User.count({
-		where: {
-			userRole: {
-				[Op.eq]: userRoles.ADMIN,
-			},
-		},
-	});
-	console.log(adminCount, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	if (adminCount === 0) {
-		const adminInfo = {
-			name: "Umarov Ali",
-			username: "admin",
-			password: "20230101",
-			userRole: "ADMIN",
-		};
-		await User.create(adminInfo);
-	}
+  if (countFoods === 0) {
+    await Food.bulkCreate(foodsJson);
+  }
+  const adminCount = await User.count({
+    where: {
+      userRole: {
+        [Op.eq]: userRoles.ADMIN,
+      },
+    },
+  });
+  if (adminCount === 0) {
+    const adminInfo = {
+      name: "Umarov Ali",
+      username: "admin",
+      password: "20230101",
+      userRole: "ADMIN",
+    };
+    await User.create(adminInfo);
+  }
 });
