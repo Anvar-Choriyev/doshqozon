d0fb3249b31124c303ea8c474c7d2fa1b059d198
Abdumalik <e.a1994@mail.ru>
2022-12-27 09:55:47 +0500

    update

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const orderRouter = require("../server/modules/order/orderRouter");
 const orderItemRouter = require("../server/modules/orderItem/orderItemRouter");
 app.use(express.json());
 app.use(require("cors")());
+app.use(express.static("./static", {}));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
