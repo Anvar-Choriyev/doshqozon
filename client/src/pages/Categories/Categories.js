c24fbcbfd2728e2fbce3a718e36531b89d1fef66
Anvar <choriyevanvar08@gmai.com>
2022-12-27 21:02:47 +0500

    update

--- client/src/pages/Categories/Categories.js
+++ client/src/pages/Categories/Categories.js
@@ -11,11 +11,8 @@ import ColdDrinks from "../../assets/images/cold-drinks.png";
 import { useEffect, useState } from "react";
 import http from "../../utils/axios-instance";
 import { toast } from "react-toastify";
-<<<<<<< HEAD
-=======
 import { Link } from "react-router-dom";
 import Card from "../../components/Generics/Card/Card"
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 
 const Categories = () => {
 	const categoryImg = [
@@ -25,19 +22,11 @@ const Categories = () => {
 		},
 		{
 			id: 2,
-<<<<<<< HEAD
-			src: Bread,
-		},
-		{
-			id: 3,
-			src: Salad,
-=======
 			src: Salad,
 		},
 		{
 			id: 3,
 			src: Bread,
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 		},
 		{
 			id: 4,
@@ -49,13 +38,6 @@ const Categories = () => {
 		},
 	];
 	const [categories, setCategories] = useState([]);
-<<<<<<< HEAD
-	useEffect(() => {
-		getAllCategories();
-	}, []);
-=======
-
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 	const getAllCategories = async () => {
 		try {
 			const res = await http({
@@ -67,44 +49,19 @@ const Categories = () => {
 			toast.error(error.response.data.message);
 		}
 	};
-<<<<<<< HEAD
-=======
 	useEffect(() => {
 		getAllCategories();
 	}, []);
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 	const categoryArr = [];
 	categoryImg.forEach((img) =>
 		categories?.forEach((ctg) => {
 			if (img.id === ctg.id) {
-<<<<<<< HEAD
-				categoryArr.push({ src: img.src, name: ctg.name });
-=======
 				categoryArr.push({ src: img.src, name: ctg.name, id: ctg.id });
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 			}
 		})
 	);
 	return (
 		<>
-<<<<<<< HEAD
-			<Layout>
-				<div className={styles.mainText}>
-					<p className={`title ${styles.categoryText}`}>Kategoriyalar</p>
-					<button className={`main-text ${styles.charge}`}>
-						Charge customer
-					</button>
-				</div>
-				<div className={styles.categories}>
-					{categoryArr.map((ctg) => (
-						<div className={styles.categoryBox}>
-							<img src={ctg.src} alt="" />
-							<p className="name-text">{ctg.name}</p>
-						</div>
-					))}
-				</div>
-			</Layout>
-=======
 			<div className={styles.mainText}>
 				<p className={`title ${styles.categoryText}`}>Kategoriyalar</p>
 				<button className={`main-text ${styles.charge}`}>
@@ -122,7 +79,6 @@ const Categories = () => {
 					/>
 				))}
 			</div>
->>>>>>> 9528af4f1e313227fc48d43993e84e2920122c85
 		</>
 	);
 };
