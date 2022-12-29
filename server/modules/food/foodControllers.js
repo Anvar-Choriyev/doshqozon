a3463c1d2ee72a2a8524c97b375bd4c7f4c11324
Shodiyor Ergashev <mybusinesskebap@gmail.com>
2022-12-29 14:21:39 +0500

    bread addes

--- server/modules/food/foodControllers.js
+++ server/modules/food/foodControllers.js
@@ -5,27 +5,38 @@ const AppError = require("../../core/utils/AppError");
 const foods = require("../../core/constants/foods.json");
 
 exports.getFoodById = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const mainFood = await Food.findByPk(id);
-  if (id == 1 || id == 2 || id == 3 || id == 4) {
-    console.log("Logga kirdi");
-    additional = await Food.findAll({
-      where: {
-        categoryId: {
-          [Op.eq]: 6,
-        },
-      },
-    });
-  }
-  res.json({
-    status: "success",
-    message: "Food by Id",
-    error: null,
-    data: {
-      mainFood,
-      extra: {
-        additional,
-      },
-    },
-  });
+	const { id } = req.params;
+	// console.log(typeof id, "AAAAAAAAAAAAAA");
+	const mainFood = await Food.findByPk(id);
+	let additional;
+	if (id == 1 || id == 2 || id == 3 || id == 4) {
+		console.log("Logga kirdi");
+		additional = await Food.findAll({
+			where: {
+				categoryId: {
+					[Op.eq]: 6,
+				},
+			},
+		});
+	}
+	if (id == 9) {
+		additional = await Food.findAll({
+			where: {
+				categoryId: {
+					[Op.eq]: 3,
+				},
+			},
+		});
+	}
+	res.json({
+		status: "success",
+		message: "Food by Id",
+		error: null,
+		data: {
+			mainFood,
+			extra: {
+				additional,
+			},
+		},
+	});
 });
