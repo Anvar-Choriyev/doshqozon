0455ce9fb608826bd659430e580e9f30f30d716b
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-12-29 20:10:09 +0500

    img

--- client/src/components/Modal/Content.jsx
+++ client/src/components/Modal/Content.jsx
@@ -47,7 +47,7 @@ const Content = (props) => {
 				<div key={i.id}>
 					<div className={style.item}>
 						<div className={style.imgDiv}>
-							<img src={`https://img.alltor.me/img${i.url}`} alt="" />
+							<img height="64px" width="64px" src={`https://img.alltor.me/img${i.url}`} alt="" />
 							<p>{i.name}</p>
 						</div>
 						<div className={style.itemChange}>
